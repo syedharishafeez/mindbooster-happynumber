e05dee6d9d351335ab34708e5b72ea1ce7b9a12d
syedharishafeez <syed_haris98@yahoo.com>
2020-06-29 16:53:08 +0500

    Initial Commit

---
+++ happynumber.js
@@ -0,0 +1,36 @@
+let number = 29;
+
+let stringNumber = "" + number;
+
+let comingNumber;
+
+let arrayNumber = [stringNumber];
+
+let firstTime = true;
+
+let checkArray = true;
+
+while (firstTime || (parseInt(comingNumber) !== 1 && checkArray)) {
+  let maintainNumber = 0;
+  if (firstTime) {
+    comingNumber = stringNumber;
+    firstTime = false;
+  }
+  for (let i = 0; i < comingNumber.length; i++) {
+    maintainNumber += parseInt(comingNumber[i]) ** 2;
+    // console.log("maintainNumber = ", maintainNumber);
+  }
+  comingNumber = "" + maintainNumber;
+  if (arrayNumber.includes(comingNumber)) {
+    checkArray = false;
+  } else {
+    arrayNumber.push(comingNumber);
+  }
+  console.log("arrayNumber = ", arrayNumber);
+}
+
+if (parseInt(comingNumber) === 1) {
+  console.log(number, " is a happy number");
+} else {
+  console.log(number, " is not a happy number");
+}
