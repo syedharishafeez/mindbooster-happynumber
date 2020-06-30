bb4964e478c2e96b9339e359033ff3db236d6f9b
syedharishafeez <syed_haris98@yahoo.com>
2020-06-30 20:55:02 +0500

    remove unused variables

--- happynumber.js
+++ happynumber.js
@@ -1,29 +1,27 @@
 let number = 29;
 
-let comingNumber = number;
-
 let arrayNumber = [number];
 
 let checkArray = true;
 
-while (parseInt(comingNumber) !== 1 && checkArray) {
+while (parseInt(number) !== 1 && checkArray) {
   let maintainNumber = 0;
-  let divideNumber = comingNumber;
+  let divideNumber = number;
   while (divideNumber / 10 !== 0) {
     maintainNumber = maintainNumber + (divideNumber % 10) ** 2;
     divideNumber = parseInt(divideNumber / 10);
   }
-  comingNumber = maintainNumber;
-  if (arrayNumber.includes(comingNumber)) {
+  number = maintainNumber;
+  if (arrayNumber.includes(number)) {
     checkArray = false;
   } else {
-    arrayNumber.push(comingNumber);
+    arrayNumber.push(number);
   }
   console.log(arrayNumber);
 }
 
-if (parseInt(comingNumber) === 1) {
-  console.log(number, " is a happy number");
+if (parseInt(number) === 1) {
+  console.log(arrayNumber[0], " is a happy number");
 } else {
-  console.log(number, " is not a happy number");
+  console.log(arrayNumber[0], " is not a happy number");
 }
